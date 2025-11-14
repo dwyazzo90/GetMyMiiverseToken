10f370e4618d6ff72e64f257a0dc0661859f307d
davey stone but bfdi mouth <118952873+davidsosa2022@users.noreply.github.com>
2025-11-14 10:49:46 -0500

    Initial commit of my Wii U homebrew app

---
+++ src/main.cpp
@@ -0,0 +1,196 @@
+#include <coreinit/thread.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <time.h>
+#include <coreinit/time.h> // For OSCalendarTime
+#include <coreinit/debug.h>
+#include <whb/log.h>
+#include <whb/log_console.h>
+#include <whb/proc.h>
+#include <string.h>
+#include <nn/ac.h>
+#include <nn/act.h>
+#include <nsysnet/nssl.h>
+
+#include <stdbool.h>
+#include <stdio.h>
+
+typedef unsigned int u8;
+typedef unsigned int u32;
+typedef unsigned int u64;
+typedef unsigned int s32;
+
+struct InitializeParam
+{
+    u32 flags;
+    u32 reportTypes;
+    u8 *work;
+    u32 workSize;
+    const void *args;
+    u32 argsSize;
+    u8 reserved[40];
+};
+
+extern "C" void InitializeParam_ctor(InitializeParam *thisPtr) asm("__ct__Q3_2nn3olv15InitializeParamFv");
+
+extern "C" u32 InitializeParam_SetFlags(InitializeParam *thisPtr, u32 flags) asm("SetFlags__Q3_2nn3olv15InitializeParamFUi");
+
+extern "C" u32 InitializeParam_SetWork(InitializeParam *thisPtr, u8 *work, u32 size) asm("SetWork__Q3_2nn3olv15InitializeParamFPUcUi");
+
+extern "C" u32 InitializeParam_SetSysArgs(InitializeParam *thisPtr, const void *args, u32 size) asm("SetSysArgs__Q3_2nn3olv15InitializeParamFPCvUi");
+
+extern "C" u32 InitializeParam_SetReportTypes(InitializeParam *thisPtr, u32 flags) asm("SetReportTypes__Q3_2nn3olv15InitializeParamFUi");
+
+extern "C" u32 nnOlvInitialize(const InitializeParam *param) asm("Initialize__Q2_2nn3olvFPCQ3_2nn3olv15InitializeParam");
+
+extern "C" u32 nnOlvFinalize(void) asm("Finalize__Q2_2nn3olvFv");
+
+extern "C" uint32_t GetServiceToken(char *buf, unsigned int bufSize) asm("GetServiceToken__Q2_2nn3olvFPcUi");
+
+extern "C" uint32_t GetParamPack(char *buf, unsigned int bufSize) asm("GetParamPack__Q2_2nn3olvFPcUi");
+
+extern "C" uint32_t GetUserAgent(char *buf, unsigned int bufSize) asm("GetUserAgent__Q2_2nn3olvFPcUi");
+
+void InitializeMiiverse()
+{
+    alignas(8) InitializeParam paramStorage;
+    InitializeParam *param = &paramStorage;
+
+    InitializeParam_ctor(param);
+
+    static u8 workBuffer[512 * 1024];
+
+    InitializeParam_SetFlags(param, 0);
+    InitializeParam_SetWork(param, workBuffer, sizeof(workBuffer));
+
+    u32 rc = nnOlvInitialize(param);
+
+    // Discovery request success
+    if (rc != 0x01100080)
+    {
+        WHBLogPrintf("[nn::olv] nn::olv::Initialize failed (rc=%08X)\n", rc);
+    }
+    else
+    {
+        WHBLogPrintf("[nn::olv] nn::olv::Initialize SUCCESS! -> (rc=%08X)\n", rc);
+
+        char tokenBuf[520] = {};
+        u32 rcTok = GetServiceToken(tokenBuf, sizeof(tokenBuf));
+
+        if (rcTok != 0x01100080)
+        {
+            WHBLogPrintf("[nn::olv] nn::olv::GetServiceToken failed (rc=%08X)\n", rcTok);
+        }
+        else
+        {
+            WHBLogPrintf("[nn::olv] nn::olv::GetServiceToken SUCCESS! -> (rc=%08X)\n", rcTok);
+            WHBLogPrintf("[nn::olv] Service Token: %s\n", tokenBuf);
+        }
+
+        char packBuf[520] = {};
+        u32 rcPack = GetParamPack(packBuf, sizeof(packBuf));
+
+        if (rcPack != 0x01100080)
+        {
+            WHBLogPrintf("[nn::olv] nn::olv::GetParamPack failed (rc=%08X)\n", rcPack);
+        }
+        else
+        {
+            WHBLogPrintf("[nn::olv] nn::olv::GetParamPack SUCCESS! -> (rc=%08X)\n", rcPack);
+            WHBLogPrintf("[nn::olv] Param Pack: %s\n", packBuf);
+        }
+
+        WHBLogPrintf("Saving Miiverse auth data to SD Card...\n");
+        OSSleepTicks(OSMillisecondsToTicks(10));
+
+        // Get current user's PID
+        nn::act::PrincipalId pid = nn::act::GetPrincipalId();
+
+        // Get current user's NNID
+        char accid[17] = {};
+        nn::act::GetAccountId(accid);
+
+        // Create folder if it doesn't exist
+        const char *folderPath = "fs:/vol/external01/miiverse_auth";
+        mkdir(folderPath, 0777); // create folder if missing
+
+        // Get current timestamp
+        time_t now = time(NULL);
+        struct tm t;
+        localtime_r(&now, &t); // fills struct tm with local time
+
+        char filePath[512];
+        snprintf(filePath, sizeof(filePath),
+                 "%s/token_%s_%04d%02d%02d%02d%02d%02d.txt",
+                 folderPath,
+                 accid,
+                 t.tm_year + 1900,
+                 t.tm_mon + 1,
+                 t.tm_mday,
+                 t.tm_hour,
+                 t.tm_min,
+                 t.tm_sec);
+
+        // Write to file
+        FILE *fp = fopen(filePath, "w");
+        if (fp)
+        {
+            fprintf(fp, "Service Token: %s\n", tokenBuf);
+            fprintf(fp, "Param Pack: %s\n", packBuf);
+            fprintf(fp, "User ID: %s\n", accid);
+            fprintf(fp, "Principal ID: %u\n", pid);
+            fclose(fp);
+
+            WHBLogPrintf("Successfully saved Miiverse auth data to SD Card:\n");
+            WHBLogPrintf("%s\n", filePath);
+        }
+        else
+        {
+            WHBLogPrintf("Failed to write SD Card file:\n");
+            WHBLogPrintf("%s\n", filePath);
+        }
+        OSSleepTicks(OSMillisecondsToTicks(10));
+    }
+}
+
+int main(int argc, char **argv)
+{
+    WHBProcInit();
+    WHBLogConsoleInit();
+
+    WHBLogPrintf("Initializing network...\n");
+    WHBLogConsoleDraw();
+    OSSleepTicks(OSMillisecondsToTicks(50));
+
+    nn::ac::Initialize();
+    nn::ac::ConfigIdNum configId;
+    nn::ac::GetStartupId(&configId);
+    nn::ac::Connect(configId);
+
+    NSSLInit();
+
+    WHBLogPrintf("Initializing account...\n");
+    WHBLogConsoleDraw();
+    OSSleepTicks(OSMillisecondsToTicks(50));
+
+    nn::act::Initialize();
+
+    WHBLogPrintf("[nn::olv] Starting Miiverse initialization...\n");
+    WHBLogConsoleDraw();
+    OSSleepTicks(OSMillisecondsToTicks(50));
+
+    InitializeMiiverse();
+
+    while (WHBProcIsRunning())
+    {
+        WHBLogConsoleDraw();
+        OSSleepTicks(OSMillisecondsToTicks(50));
+    }
+
+    nnOlvFinalize();
+    NSSLFinish();
+    nn::act::Finalize();
+    nn::ac::Finalize();
+
+    return 0;
+}
